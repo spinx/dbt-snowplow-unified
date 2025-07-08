c80bb1b3de340c597b9aa4e3b4b77599599fd40a
Damjan Znidarsic <damjan@businessoffashion.com>
2025-07-08 20:24:29 +0200

    added

--- integration_tests/models/expected/snowplow_unified_sessions_expected_stg.sql
+++ integration_tests/models/expected/snowplow_unified_sessions_expected_stg.sql
@@ -55,8 +55,8 @@ SELECT
     ,start_tstamp
     ,end_tstamp
 
-    -- hard-coding due to non-deterministic outcome from row_number for Redshift/Postgres/databricks
-    {% if target.type in ['redshift', 'postgres', 'databricks'] -%}
+    -- hard-coding due to non-deterministic outcome from row_number for Redshift/Postgres/databricks/duckdb
+    {% if target.type in ['redshift', 'postgres', 'databricks', 'duckdb'] -%}
     , case when event_id = 'b3eca04e-d277-45e0-9c7c-76dc7e8c16ff' then 'true base' else app_id end as app_id
     {% else %}
     , app_id
@@ -218,8 +218,8 @@ SELECT
     ,fatal_app_errors
     ,useragent
 
--- hard-coding due to non-deterministic outcome from row_number for Redshift/Postgres/databricks
-{% if target.type in ['redshift', 'postgres', 'databricks'] -%}
+-- hard-coding due to non-deterministic outcome from row_number for Redshift/Postgres/databricks/duckdb
+{% if target.type in ['redshift', 'postgres', 'databricks', 'duckdb'] -%}
   , case when event_id = 'b3eca04e-d277-45e0-9c7c-76dc7e8c16ff' then '3cfe1cd4-a20e-4fc7-952a-a5cb7f7d063f' else event_id end as event_id
   , case when event_id2 = 'b3eca04e-d277-45e0-9c7c-76dc7e8c16ff' then '3cfe1cd4-a20e-4fc7-952a-a5cb7f7d063f' else event_id2 end as event_id2
 {% else %}
